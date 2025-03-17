b535e2b11a5a37087f248ed4b6d145a78ea68383
shiru84 <64347060+shiru84@users.noreply.github.com>
2025-03-17 09:42:40 +0000

    Add files via upload

---
+++ src/test/java/CUG/CUG2023_1586/CUG2023_1586.java
@@ -0,0 +1,241 @@
+package CUG.CUG2023_1586;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.Duration;
+
+//48 tests
+
+public class CUG2023_1586 {
+static WebDriver driver;
+WebDriverWait wait;
+static XSSFWorkbook workbook;
+    static XSSFSheet sheet;
+    static String URL;
+    static String COPY;
+    static int wb =0;
+    static int r =0;
+    static int c =1;
+    static String XLSXpath = System.getProperty("user.dir")+"/src/test/java/CUG/CUG2023_1586/CUGtestfile.xlsx";
+    public WebElement copyHolder;
+    public static String positionOne = "//*[@id=\"product\"]/div/div[2]/div[2]/ul/li/div/div[2]/div[2]/div[1]/div[1]";
+//            "/html/body/div[1]/section[3]/div/div[2]/div[4]/div[1]/div/div[2]/div[2]/ul/li/div/div[2]/div[2]/div[1]";
+    public static String positionTwo = "//*[@id=\"product\"]/div/div[2]/div[2]/ul/li/div/div[2]/div[2]/div[1]/div[2]";
+//        "/html/body/div[1]/section[3]/div/div[2]/div[4]/div[1]/div/div[2]/div[2]/ul/li/div/div[2]/div[2]/div[1]/div[2]/p/span";
+String preURL1 = "https://www.samsung.com/uk/multistore/uk_student/";
+    String preURL2 = "https://www.samsung.com/uk/multistore/uk_networks/";
+    @BeforeTest
+    void Setup() {
+        //C:\Program Files\Google\Chrome\Application
+        //chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\Users\mykola.r\IdeaProjects\RmoteF\src\test\resources\ChromeProfile"
+        System.setProperty("webdriver.chrome.driver", "C:/Users/mykola.r/IdeaProjects/RmoteF/src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("debuggerAddress", "127.0.0.1:9222");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get(preURL1);
+
+    }
+void getFromExcell(String position) throws IOException {
+        workbook = new XSSFWorkbook(XLSXpath);
+        sheet = workbook.getSheetAt(wb);
+        URL = sheet.getRow(r).getCell(c).getStringCellValue();
+        driver.get(URL);
+    wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/div[1]/div[4]/nav/div/div")));
+    COPY = sheet.getRow(r).getCell(c+1).getStringCellValue();
+    wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/div[1]/section[3]/div/div[2]/div[4]/div[1]/div/div[2]/div[2]/ul/li/div/div[2]/div[2]/div[1]")));
+    copyHolder = driver.findElement(By.xpath(position));
+
+
+}
+    void writeFile() throws IOException {
+        FileOutputStream fos = new FileOutputStream(XLSXpath);
+        workbook.write(fos);
+        fos.close();
+        workbook.close();
+
+    }
+    void writeResult (WebElement CopyHolder, Integer cValue) throws IOException {
+        FileInputStream fis = new FileInputStream(XLSXpath);
+        workbook = new XSSFWorkbook(fis);
+        sheet = workbook.getSheetAt(wb);
+        Row row = sheet.getRow(r);
+        row.createCell(cValue).setCellValue(CopyHolder.getText());
+        writeFile();
+
+    }
+    public static void takeScreenshot(Integer ROW) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        // Cast WebDriver instance to TakesScreenshot to access screenshot functionality
+        TakesScreenshot screenshotTaker = (TakesScreenshot) driver;
+
+        // Capture the screenshot and store it as a file
+        File srcFile = screenshotTaker.getScreenshotAs(OutputType.FILE);
+
+        // Define the destination file path
+        File destFile = new File( System.getProperty("user.dir")+"/src/test/java/CUG/CUG2023_1586/Screenshots/" + ROW + ".jpg");
+
+        try {
+            // Copy the screenshot to the specified location
+            FileUtils.copyFile(srcFile, destFile);
+            System.out.println("Screenshot saved as " + destFile.getAbsolutePath());
+        } catch (IOException e) {
+            System.out.println("Failed to save screenshot: " + e.getMessage());
+        }
+    }
+    public void TEST() throws IOException {
+        r = r + 1;
+//        getFromExcell(positionOne);
+//        if(copyHolder.getText().contains(COPY)){
+//            writeResult(copyHolder, 3);
+//        } else {
+//        getFromExcell(positionTwo);
+//                if(copyHolder.getText().contains(COPY)){
+//                writeResult(copyHolder, 4);
+//                } else {
+//                    takeScreenshot(r);
+//            }
+//
+//        }
+        try {
+            getFromExcell(positionOne);
+            writeResult(copyHolder, 3);
+            Assert.assertTrue(copyHolder.getText().contains(COPY));
+        } catch (Error error) {
+            getFromExcell(positionTwo);
+            writeResult(copyHolder, 4);
+            Assert.assertTrue(copyHolder.getText().contains(COPY));
+        } finally {
+            takeScreenshot(r);
+        }
+    }
+    @Test
+    void test001() throws IOException {
+        TEST();
+        
+    }
+    @Test    void test002() throws IOException { TEST();  }
+    @Test    void test003() throws IOException { TEST();  }
+    @Test    void test004() throws IOException { TEST();  }
+    @Test    void test005() throws IOException { TEST();  }
+    @Test    void test006() throws IOException { TEST();  }
+    @Test    void test007() throws IOException { TEST();  }
+    @Test    void test008() throws IOException { TEST();  }
+    @Test    void test009() throws IOException { TEST();  }
+    @Test    void test010() throws IOException { TEST();  }
+    @Test    void test011() throws IOException { TEST();  }
+    @Test    void test012() throws IOException { TEST();  }
+    @Test    void test013() throws IOException { TEST();  }
+    @Test    void test014() throws IOException { TEST();  }
+    @Test    void test015() throws IOException { TEST();  }
+    @Test    void test016() throws IOException { TEST();  }
+    @Test    void test017() throws IOException { TEST();  }
+    @Test    void test018() throws IOException { TEST();  }
+    @Test    void test019() throws IOException { TEST();  }
+    @Test    void test020() throws IOException { TEST();  }
+    @Test    void test021() throws IOException { TEST();  }
+    @Test    void test022() throws IOException { TEST();  }
+    @Test    void test023() throws IOException { TEST();  }
+    @Test    void test024() throws IOException { TEST();  }
+    @Test    void test025() throws IOException { TEST();  }
+    @Test    void test026() throws IOException { TEST();  }
+    @Test    void test027() throws IOException { TEST();  }
+    @Test    void test028() throws IOException { TEST();  }
+    @Test    void test029() throws IOException { TEST();  }
+    @Test    void test030() throws IOException { TEST();  }
+    @Test    void test031() throws IOException { TEST();  }
+    @Test    void test032() throws IOException { TEST();  }
+//    @Test    void test033() throws IOException { TEST();  }
+//    @Test    void test034() throws IOException { TEST();  }
+//    @Test    void test035() throws IOException { TEST();  }
+//    @Test    void test036() throws IOException { TEST();  }
+//    @Test    void test037() throws IOException { TEST();  }
+//    @Test    void test038() throws IOException { TEST();  }
+//    @Test    void test039() throws IOException { TEST();  }
+//    @Test    void test040() throws IOException { TEST();  }
+//    @Test    void test041() throws IOException { TEST();  }
+//    @Test    void test042() throws IOException { TEST();  }
+//    @Test    void test043() throws IOException { TEST();  }
+//    @Test    void test044() throws IOException { TEST();  }
+//    @Test    void test045() throws IOException { TEST();  }
+//    @Test    void test046() throws IOException { TEST();  }
+//    @Test    void test047() throws IOException { TEST();  }
+//    @Test    void test048() throws IOException { TEST();  }
+//    @Test    void test049() throws IOException { TEST();  }
+//    @Test    void test050() throws IOException { TEST();  }
+//    @Test    void test051() throws IOException { TEST();  }
+//    @Test    void test052() throws IOException { TEST();  }
+//    @Test    void test053() throws IOException { TEST();  }
+//    @Test    void test054() throws IOException { TEST();  }
+//    @Test    void test055() throws IOException { TEST();  }
+//    @Test    void test056() throws IOException { TEST();  }
+//    @Test    void test057() throws IOException { TEST();  }
+//    @Test    void test058() throws IOException { TEST();  }
+//    @Test    void test059() throws IOException { TEST();  }
+//    @Test    void test060() throws IOException { TEST();  }
+//    @Test    void test061() throws IOException { TEST();  }
+//    @Test    void test062() throws IOException { TEST();  }
+//    @Test    void test063() throws IOException { TEST();  }
+//    @Test    void test064() throws IOException { TEST();  }
+//    @Test    void test065() throws IOException { TEST();  }
+//    @Test    void test066() throws IOException { TEST();  }
+//    @Test    void test067() throws IOException { TEST();  }
+//    @Test    void test068() throws IOException { TEST();  }
+//    @Test    void test069() throws IOException { TEST();  }
+//    @Test    void test070() throws IOException { TEST();  }
+//    @Test    void test071() throws IOException { TEST();  }
+//    @Test    void test072() throws IOException { TEST();  }
+//    @Test    void test073() throws IOException { TEST();  }
+//    @Test    void test074() throws IOException { TEST();  }
+//    @Test    void test075() throws IOException { TEST();  }
+//    @Test    void test076() throws IOException { TEST();  }
+//    @Test    void test077() throws IOException { TEST();  }
+//    @Test    void test078() throws IOException { TEST();  }
+//    @Test    void test079() throws IOException { TEST();  }
+//    @Test    void test080() throws IOException { TEST();  }
+//    @Test    void test081() throws IOException { TEST();  }
+//    @Test    void test082() throws IOException { TEST();  }
+//    @Test    void test083() throws IOException { TEST();  }
+//    @Test    void test084() throws IOException { TEST();  }
+//    @Test    void test085() throws IOException { TEST();  }
+//    @Test    void test086() throws IOException { TEST();  }
+//    @Test    void test087() throws IOException { TEST();  }
+//    @Test    void test088() throws IOException { TEST();  }
+//    @Test    void test089() throws IOException { TEST();  }
+//    @Test    void test090() throws IOException { TEST();  }
+//    @Test    void test091() throws IOException { TEST();  }
+//    @Test    void test092() throws IOException { TEST();  }
+//    @Test    void test093() throws IOException { TEST();  }
+//    @Test    void test094() throws IOException { TEST();  }
+//    @Test    void test095() throws IOException { TEST();  }
+//    @Test    void test096() throws IOException { TEST();  }
+//    @Test    void test097() throws IOException { TEST();  }
+//    @Test    void test098() throws IOException { TEST();  }
+//    @Test    void test099() throws IOException { TEST();  }
+//    @Test    void test100() throws IOException { TEST();  }
+
+
+
+
+}
+
